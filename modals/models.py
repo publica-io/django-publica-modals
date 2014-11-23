ff7e3a50287716ffd09f9cafe5bc5cc2d3a16a97
Daryl Antony <daryl@commoncode.com.au>
2014-11-24 02:13:12 +1100

    refactor harshads crazy modals

--- modals/models.py
+++ modals/models.py
@@ -1,5 +1,3 @@
-from django.db import models
-from django.contrib.contenttypes import generic
 
 from polymorphic import PolymorphicModel
 from entropy.mixins import (
@@ -7,8 +5,6 @@ from entropy.mixins import (
 )
 from attrs.mixins import GenericAttrMixin
 from templates.mixins import TemplateMixin
-
-from .settings import CONTENT_MODELS
 
 try:
     from images.mixins import ImageMixin
@@ -21,19 +17,5 @@ except ImportError:
 class Modal(PolymorphicModel, GenericAttrMixin, EnabledMixin, SlugMixin,
             TextMixin, TitleMixin, TemplateMixin, ImageMixin):
 
-    is_bootstrapped = models.BooleanField(default=False)
-
-    # def get_slug(self):
-    #     return self.slug if self.slug else ''
-
-
-class ModalAspect(PolymorphicModel, EnabledMixin):
-
-    modal = models.ForeignKey('Modal')
-    content_type = models.ForeignKey(
-        'contenttypes.ContentType',
-        limit_choices_to={'model__in': CONTENT_MODELS},
-    )
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    pass
 
--- modals/templatetags/modals_tags.py
+++ modals/templatetags/modals_tags.py
@@ -6,19 +6,10 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def bootstrapped_modals(context, slug=None):
-    '''
-    Render all the Modals on the page that are marked `is_bootstrapped = True`
-    '''
-    if slug:
-        modals = Modal.objects.filter(is_bootstrapped=True, slug=slug)
-
+def bootstrap_modal(context, slug):
+    try:
+        modal = Modal.objects.get(slug=slug)
+    except Modal.DoesNotExist:
+        return ''
     else:
-        modals = Modal.objects.filter(is_bootstrapped=True)
-
-    html = []
-    for modal in modals:
-        html.append(modal.render(context))
-
-    return ''.join(html)
-
+        return modal.render(context)
